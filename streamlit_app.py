1f254c10fff6cfe8e806e84d912c973208cb587a
arcticat500 <142036134+arcticat500@users.noreply.github.com>
2023-08-22 13:46:39 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
 
 
 # write your own comment -what does the next line do? answer: next line is like a function to remove the raw data response on screen
