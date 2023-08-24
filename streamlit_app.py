a3b59e6c422861d9645c16c7f6afea8ab40b1d5f
arcticat500 <142036134+arcticat500@users.noreply.github.com>
2023-08-24 15:07:22 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,3 +31,4 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do? answer: tosses the normlized data into a table format
 streamlit.dataframe(fruityvice_normalized)
 
+import snowlake.connector
